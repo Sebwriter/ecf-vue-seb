7b05512567d39a669f2d05c47ed745ad2e872506
Sébastien Montibert <seb.montibert@gmail.com>
2022-04-20 16:34:11 +0200

    ajout de bonus

--- src/views/CardDetails.vue
+++ src/views/CardDetails.vue
@@ -1,87 +1,89 @@
 <template>
     <div id="main-wrapper">
-					<div class="wrapper style2">
-						<div class="inner">
-							<div class="container">
-								<div class="row">
-									<div class="col-8 col-12-medium">
-										<div id="content">
+        <div class="wrapper style2">
+            <div class="inner">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-8 col-12-medium">
+                            <div id="content">
 
-											<!-- Content -->
+                                <!-- Content -->
 
-												<article>
-													<header class="major">
-														<h2>{{carte.name}}</h2>
-														<p>{{carte.artist}}</p>
-													</header>
+                                <article>
+                                    <header class="major">
+                                        <h2>{{ carte.name }}</h2>
+                                        <p>{{ carte.artist }}</p>
+                                    </header>
 
-													<span class="image featured">
-														<img :src="carte.imageUrl" alt="" />
-													</span>
+                                    <span class="image featured">
+                                        <img :src="carte.imageUrl" alt="" />
+                                    </span>
 
-													<p>{{carte.text}}</p>
-												</article>
+                                    <!-- au cas où l'image serait manquante -->
+                                    <h4 style="width:50%; margin-left:25%; margin-bottom:10px" v-if="!carte.imageUrl">
+                                        Sorry, our wizards are purchasing this image !
+                                    </h4>
+                                    <h4 style="width:50%; margin-left:25%; margin-bottom:40px" v-if="!carte.imageUrl">
+                                        Please excuse us for the incident !
+                                    </h4>
 
-										</div>
-									</div>
-									<div class="col-4 col-12-medium">
-										<div id="sidebar">
+                                    <p>{{ carte.text }}</p>
+                                </article>
 
-											<!-- Sidebar -->
+                            </div>
+                        </div>
+                        <div class="col-4 col-12-medium">
+                            <div id="sidebar">
 
-												<section>
-													<header class="major">
-														<h2>Infos</h2>
-													</header>
-													<p>set name: <b>{{carte.setName}}</b></p>
-													<p >rarity:  <b>{{carte.rarity}}</b></p>
-													<p >toughness:  <b>{{carte.toughness}}</b></p>
+                                <!-- Sidebar -->
 
-													<span class="button alt icon ">
-														{{'Card power :  '+ power}}
-									
-													</span>
-												</section>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+                                <section>
+                                    <header class="major">
+                                        <h2>Infos</h2>
+                                    </header>
+                                    <p>set name: <b>{{ carte.setName }}</b></p>
+                                    <p>rarity: <b>{{ carte.rarity }}</b></p>
+                                    <p>toughness: <b>{{ carte.toughness }}</b></p>
 
-				</div>
+                                    <span class="button alt icon ">
+                                        {{ 'Card power : ' + power }}
+
+                                    </span>
+                                </section>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
 </template>
 
 <script>
 import axios from 'axios'
 
 export default {
-  name: 'CardDetails',
-  components: {
-    
-  },
-  data:()=>({
-carte:{},
-power:""
-  }),
-  methods:{
-async powering(lvl){
-for(let i=0;i<lvl;i++){
-    this.power=this.power+"🔥"
+    name: 'CardDetails',
+    components: {
 
-}
-}
-  },
-  async created(){
-      const {cardId} = this.$route.params
-    const laCarte = await axios.get('https://api.magicthegathering.io/v1/cards/'+ cardId)
-    this.carte=laCarte.data.card
-    console.log(this.carte)
-    for(let i=0;i<laCarte.data.card.power;i++){
-    this.power=this.power+"🔥"
+    },
 
-}
-  }
+    data: () => ({
+        carte: {},
+        power: ""
+    }),
+    async created() {
+        const { cardId } = this.$route.params
+        const laCarte = await axios.get('https://api.magicthegathering.io/v1/cards/' + cardId)
+        this.carte = laCarte.data.card
+        //console.log(this.carte)
+
+        for (let i = 0; i < laCarte.data.card.power; i++) {
+            this.power = this.power + "🔥"
+        }
+        if (this.power == "") { this.power = "None" }
+    }
 }
 </script>
 
--- src/views/CollectionView.vue
+++ src/views/CollectionView.vue
@@ -14,7 +14,8 @@
 											<div v-for="(carte,idx) in cartes" :key="idx" class='card-list'>
 												<div class='card-title'><b>{{'# '+(idx+1)}}</b> {{carte.name}}</div>
 												<div class='card-action'
-												@click="goToSelectedCard(carte.id)">👀</div>
+												@click="goToSelectedCard(carte.id)"
+												@mouseover="pointer()">👀</div>
 											</div>
 											
 
@@ -43,7 +44,13 @@ cartes:[]
   methods:{
 goToSelectedCard(cardId){
 	this.$router.push({name:'CardDetails', params:{cardId}})
-}
+},
+ pointer() {
+            const mesYeux = document.querySelectorAll('.card-action')
+            for (let i = 0; i < mesYeux.length; i++) {
+                mesYeux[i].style.cursor = 'pointer'
+            }
+        }
   },
   async created(){
     const lesCartes = await axios.get('https://api.magicthegathering.io/v1/cards')
@@ -54,6 +61,25 @@ goToSelectedCard(cardId){
 </script>
 
 <style scoped>
-/* @import url("./assets/css/card-list.css"); */
+div.card-list{
+    border-radius:8px;
+    padding:8px;
+    border:1px solid grey;
+    display: flex;
+    box-shadow: rgba(0, 0, 0, 0.1) 0px 4px 12px;
+    margin-bottom:4px;
+}
+
+div.card-title{
+    flex:6
+}
+
+div.card-action{
+    flex:1
+}
+div.card-action a{
+    outline: none;
+    text-decoration: none;
+}
 </style>
 
